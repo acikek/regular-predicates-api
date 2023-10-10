c4ba057d00797a4fd63a7f85a31c7d7228e88383
Skye Prince <spiralixp@gmail.com>
2023-10-10 13:19:19 -0700

    init

---
+++ src/main/java/com/acikek/predicate/RegularPredicatesMod.java
@@ -0,0 +1,22 @@
+package com.acikek.predicate;
+
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RegularPredicatesMod implements ModInitializer {
+
+    public static final String ID = "regular_predicates";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+    public static Identifier id(String path) {
+        return new Identifier(ID, path);
+    }
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
