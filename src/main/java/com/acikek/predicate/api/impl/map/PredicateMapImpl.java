72c373ee8b19c436ba339a3840c70cf51ab97ea3
Skye Prince <spiralixp@gmail.com>
2023-10-10 22:11:03 -0700

    predicate map

---
+++ src/main/java/com/acikek/predicate/api/impl/map/PredicateMapImpl.java
@@ -0,0 +1,43 @@
+package com.acikek.predicate.api.impl.map;
+
+import com.acikek.predicate.api.RegularPredicate;
+import com.acikek.predicate.api.map.PredicateMap;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.List;
+import java.util.Objects;
+
+public record PredicateMapImpl(ImmutableMap<String, RegularPredicate<?>> predicates) implements PredicateMap {
+
+    private static <T> boolean test(String name, Object value, RegularPredicate<T> predicate) {
+        Objects.requireNonNull(predicate);
+        Objects.requireNonNull(predicate.contextType());
+        try {
+            var input = predicate.contextType().cast(value);
+            return predicate.test(input);
+        }
+        catch (ClassCastException exception) {
+            throw new IllegalStateException("failed to test predicate '" + name + "'", exception);
+        }
+    }
+
+    @Override
+    public boolean test(String name, Object value) {
+        return test(name, value, predicates.get(name));
+    }
+
+    @Override
+    public boolean test(List<Object> values) {
+        var predicateList = predicates.entrySet().stream().toList();
+        for (int i = 0; i < values.size(); i++) {
+            if (i >= predicateList.size()) {
+                break;
+            }
+            var entry = predicateList.get(i);
+            if (!test(entry.getKey(), values.get(i), entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/com/acikek/predicate/api/map/PredicateMap.java
@@ -0,0 +1,25 @@
+package com.acikek.predicate.api.map;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public interface PredicateMap {
+
+    boolean test(String name, Object value);
+
+    default boolean test(Map<String, Object> map) {
+        for (var entry : map.entrySet()) {
+            if (!test(entry.getKey(), entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    boolean test(List<Object> values);
+
+    default boolean test(Object... values) {
+        return test(Arrays.stream(values).toList());
+    }
+}
