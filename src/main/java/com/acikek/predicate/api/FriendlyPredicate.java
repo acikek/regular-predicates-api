f8e57f110a84bcec8016cd7d1386c2c05beacbdc
Skye Prince <spiralixp@gmail.com>
2023-10-14 00:49:11 -0700

    guys i started on docs

--- src/main/java/com/acikek/predicate/api/FriendlyPredicate.java
+++ src/main/java/com/acikek/predicate/api/FriendlyPredicate.java
@@ -1,29 +1,40 @@
 package com.acikek.predicate.api;
 
 import com.acikek.predicate.api.serializer.RegularPredicateSerializer;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Predicate;
 
+/**
+ * A {@link RegularPredicate} with non-prefixed methods and a {@link Predicate} extension.
+ */
 public interface FriendlyPredicate<T> extends RegularPredicate<T>, Predicate<T> {
 
+    /**
+     * @see RegularPredicate#rp$contextType()
+     */
     Class<T> contextType();
 
+    /**
+     * @see RegularPredicate#rp$serializer()
+     */
     RegularPredicateSerializer<?> serializer();
 
     @Override
-    @NotNull
-    default Class<T> rp$contextType() {
+    @ApiStatus.NonExtendable
+    default @NotNull Class<T> rp$contextType() {
         return contextType();
     }
 
     @Override
-    @NotNull
-    default RegularPredicateSerializer<?> rp$serializer() {
+    @ApiStatus.NonExtendable
+    default @NotNull RegularPredicateSerializer<?> rp$serializer() {
         return serializer();
     }
 
     @Override
+    @ApiStatus.NonExtendable
     default boolean rp$test(T t) {
         return test(t);
     }
--- src/main/java/com/acikek/predicate/api/RegularPredicate.java
+++ src/main/java/com/acikek/predicate/api/RegularPredicate.java
@@ -3,16 +3,40 @@ package com.acikek.predicate.api;
 import com.acikek.predicate.api.serializer.RegularPredicateSerializer;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.Predicate;
+
+/**
+ * A boolean-valued function of one argument that can be freely serialized or deserialized
+ * via a {@link RegularPredicateSerializer}.
+ * <p>
+ * Most regular predicates should be implemented as a {@link FriendlyPredicate}.
+ * Note that the {@code rp$} prefix is used to prevent method name clashes with vanilla predicates.
+ *
+ * @param <T> the type of the input to the predicate
+ * @see RegularPredicates
+ * @see Predicate
+ */
 public interface RegularPredicate<T> {
 
+    /**
+     * @return the class of the input type
+     */
     default @NotNull Class<T> rp$contextType() {
         throw new AssertionError();
     }
 
+    /**
+     * @return the corresponding predicate serializer
+     * @see RegularPredicates
+     */
     default @NotNull RegularPredicateSerializer<?> rp$serializer() {
         throw new AssertionError();
     }
 
+    /**
+     * Evaluates the predicate on the given argument.
+     * @return {@code true} if the argument matches the predicate, otherwise {@code false}
+     */
     default boolean rp$test(T t) {
         throw new AssertionError();
     }
